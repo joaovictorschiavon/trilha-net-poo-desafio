309f3108882ed46552eadecd3b731a008f5f4955
João Victor Schiavon <schiavonj@yahoo.com.br>
2025-06-10 16:25:51 -0300

    Desafio feito

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone nokia: ");
+Smartphone nokia = new Nokia("1234", "Model 1", "imei 1", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Duoligo");
+
+Console.WriteLine("Smartphone iphone: ");
+Smartphone iphone = new Iphone("5678", "Model 2", "imei 2", 86);
+iphone.Ligar();
+iphone.InstalarAplicativo("ITunes");
